f5e52b0efe9fecab8dfe0600b6d05b79e6a80d41
Nicholas Stephen <git@nickstephen.com>
2014-03-05 23:19:33 +1100

    Implemented the android examples

--- App/src/main/java/com/nickstephen/madmine/MainActivity.java
+++ App/src/main/java/com/nickstephen/madmine/MainActivity.java
@@ -1,16 +1,44 @@
 package com.nickstephen.madmine;
 
-import android.support.v7.app.ActionBarActivity;
+import android.opengl.GLSurfaceView;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+import com.nickstephen.gamelib.opengl.OpenGLSurfaceView;
+
+public class MainActivity extends FragmentActivity {
+
+    private GLSurfaceView mGLView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLView = new OpenGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
     }
 
 
