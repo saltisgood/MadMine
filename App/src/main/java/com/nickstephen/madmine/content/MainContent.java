e09ae8f609fa7e1e094e2eedcf9ee75a48091443
Nicholas Stephen <git@nickstephen.com>
2014-03-07 22:47:56 +1100

    Compatibility changes with Game Lib

--- App/src/main/java/com/nickstephen/madmine/content/MainContent.java
+++ App/src/main/java/com/nickstephen/madmine/content/MainContent.java
@@ -1,40 +1,25 @@
 package com.nickstephen.madmine.content;
 
 import android.content.Context;
-import android.opengl.GLSurfaceView;
-import android.widget.Toast;
 
 import com.nickstephen.gamelib.opengl.Shape;
 import com.nickstephen.gamelib.opengl.layout.Container;
-import com.nickstephen.gamelib.opengl.widget.IOnClickL;
 import com.nickstephen.gamelib.opengl.widget.Square;
-import com.nickstephen.lib.misc.StatMethods;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by Nick Stephen on 7/03/14.
  */
 public class MainContent extends Container {
-    public MainContent(GLSurfaceView surface, final Context context, float width, float height) {
-        super(context, width, height);
-        this.setSurface(surface);
+    public MainContent(@NotNull final Context context, @Nullable Container parent, float width, float height, float parentOffsetX, float parentOffsetY) {
+        super(context, parent, width, height, parentOffsetX, parentOffsetY);
         //this.setBoundsSize(width + 50.0f, height + 50.0f);
         this.setUnlimitedBounds(true);
         this.setScrollable(true);
 
-        Shape square = new Square(context, 25, 25, 60);
-        square.setSurface(surface);
-        /* square.setOnClickListener(new IOnClickL() {
-            @Override
-            public void onClick(Shape shape) {
-                StatMethods.hotBread(context, "Square pressed", Toast.LENGTH_SHORT);
-            }
-        });
-        square.setOnLongClickListener(new IOnClickL() {
-            @Override
-            public void onClick(Shape shape) {
-                StatMethods.hotBread(context, "Square long pressed", Toast.LENGTH_SHORT);
-            }
-        }); */
+        Shape square = new Square(context, this, 25, 25, 60);
         this.mChildren.add(square);
     }
 
--- App/src/main/java/com/nickstephen/madmine/content/TitleScreen.java
+++ App/src/main/java/com/nickstephen/madmine/content/TitleScreen.java
@@ -14,20 +14,22 @@ import com.nickstephen.gamelib.opengl.widget.Triangle;
 import com.nickstephen.lib.misc.StatMethods;
 import com.nickstephen.madmine.util.Constants;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by Nick Stephen on 6/03/14.
  */
 public class TitleScreen extends Container {
-    public TitleScreen(GLSurfaceView surface, final Context context, float width, float height) {
-        super(context, width, height);
+    public TitleScreen(@NotNull GLSurfaceView surface, @NotNull final Context context, float width, float height) {
+        super(context, null, width, height, 0, 0);
         this.setSurface(surface);
 
-        Text titleText = new Text(context, "Mine Games", Constants.ROBOTO_FONT);
+        Text titleText = new Text(context, this, "Mine Games", Constants.ROBOTO_FONT);
         titleText.setSurface(surface);
         titleText.moveTo(0, height / 4.0f);
         this.mChildren.add(titleText);
 
-        Shape start = new Circle(context, 75, new float[] { 0.0f, 1.0f, 0.0f, 1.0f} );
+        Shape start = new Circle(context, this, 75, new float[] { 0.0f, 1.0f, 0.0f, 1.0f} );
         start.setSurface(surface);
         start.moveTo(0, -100.0f);
         start.setOnClickListener(new IOnClickL() {
@@ -45,13 +47,13 @@ public class TitleScreen extends Container {
         });
         this.mChildren.add(start);
 
-        start = new Triangle(context, 0, -100.0f, 50.0f, 0.0f, new float[] { 0.0f, 0.0f, 1.0f, 1.0f });
+        start = new Triangle(context, this, 0, -100.0f, 50.0f, 0.0f, new float[] { 0.0f, 0.0f, 1.0f, 1.0f });
         start.setSurface(surface);
         this.mChildren.add(start);
 
-        Container content = new MainContent(surface, context, 200.0f, 200.0f);
+        Container content = new MainContent(context, this, 200.0f, 200.0f, -270.0f, 100.0f);
         content.setSurface(surface);
-        content.setParentOffset(-270.0f, 100.0f);
+        //content.setParentOffset(-270.0f, 100.0f);
         this.mChildContainers.add(content);
     }
 
