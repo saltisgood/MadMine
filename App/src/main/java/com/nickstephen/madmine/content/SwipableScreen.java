c95984a5493b77fa231e775f0ff0fdd1e6e22b80
Maddino <ben.madden.13@gmail.com>
2014-04-21 23:14:23 +1000

    Thought I had finished the swiping shit. It is definitely not finished. Fucked up.

--- App/src/main/java/com/nickstephen/madmine/content/SwipableScreen.java
+++ App/src/main/java/com/nickstephen/madmine/content/SwipableScreen.java
@@ -2,17 +2,23 @@ package com.nickstephen.madmine.content;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.nickstephen.gamelib.opengl.layout.RootContainer;
 import com.nickstephen.gamelib.opengl.text.Text;
 import com.nickstephen.madmine.util.Constants;
+import com.nickstephen.madmine.util.GestureFilter;
 
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Ben on 21/04/2014.
  */
-public class SwipableScreen extends RootContainer {
+public class SwipableScreen extends RootContainer implements GestureDetector.SimpleOnGestureListener {
+    private GestureFilter detector;
+
     public SwipableScreen(@NotNull GLSurfaceView surface, @NotNull final Context context, float width, float height) {
         super(context, surface, width, height, Constants.ROBOTO_FONT);
 
@@ -22,5 +28,39 @@ public class SwipableScreen extends RootContainer {
         this.mChildren.add(titleText);
 
 
+        // Detect touched area
+        detector = new GestureFilter(this, this);
     }
-}
+
+        @Override
+        public boolean dispatchTouchEvent(MotionEvent me){
+            // Call onTouchEvent of GestureFilter class.
+            this.detector.onTouchEvent(me);
+            return super.dispatchTouchEvent(me);
+        }
+
+        @Override
+        public void onSwipe(int direction){
+            String str = "";
+            switch(direction){
+                case GestureFilter.SWIPE_RIGHT: str = "Swipe Right";
+                    break;
+                case GestureFilter.SWIPE_LEFT: str = "Swipe Left";
+                    break;
+                case GestureFilter.SWIPE_UP: str = "Swipe Up:";
+                    break;
+                case GestureFilter.SWIPE_DOWN: str = "Swipe Down";
+                    break;
+            }
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+                public void onDoubleTap(){
+            Toast.makeText(this, "Double Tap", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+    }
--- App/src/main/java/com/nickstephen/madmine/util/GestureFilter.java
+++ App/src/main/java/com/nickstephen/madmine/util/GestureFilter.java
@@ -2,6 +2,7 @@ package com.nickstephen.madmine.util;
 
 import android.app.Activity;
 import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 
 /**
@@ -59,4 +60,113 @@ public class GestureFilter extends GestureDetector.SimpleOnGestureListener{
         // 'Else do nothing, it's transparent.'
         // wtf does that mean
     }
+
+    // Sets the current mode (Transparent/Solid/Dynamic) to the argument.
+    public void setMode(int m){
+        this.mode = m;
+    }
+
+    // Returns the current mode (Transparent/Solid/Dynamic).
+    public int getMode(){
+        return this.mode;
+    }
+
+    // Turns it on or off
+    public void setEnabled(boolean status){
+        this.running = status;
+    }
+
+    // Sets the maximum distance for a movement such that it is still considered to be a swipe and not a drag or something.
+    public void setSwipeMaxDistance(int distance){
+        this.swipe_Max_Distance = distance;
+    }
+
+    // Sets the minimum distance for a movement such that it is still considered a swipe and not a tap or something.
+    public void setSwipeMinDistance(int distance){
+        this.swipe_Min_Distance = distance;
+    }
+
+    // Sets the minimum velocity for a movement such that it is still considered to be a swipe and not a really slow drag or something.
+    public void setSwipeMinVelocity(int velocity){
+        this.swipe_Min_Velocity = velocity;
+    }
+
+    // Returns the maximum distance for a movement to be considered a swipe.
+    public int getSwipeMaxDistance(){
+        return this.swipe_Max_Distance;
+    }
+
+    // Returns the minimum distance for a movement to be considered a swipe.
+    public int getSwipeMinDistance(){
+        return this.swipe_Min_Distance;
+    }
+
+    // Returns the minimum velocity for a movement to be considered a swipe.
+    public int getSwipeMinVelocity(){
+        return this.swipe_Min_Velocity;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        final float xDistance = Math.abs(e1.getX() - e2.getX());
+        final float yDistance = Math.abs(e1.getY() - e2.getY());
+
+        if((xDistance > this.swipe_Max_Distance) || (yDistance > swipe_Max_Distance))
+            return false;
+
+        velocityX = Math.abs(velocityX);
+        velocityY = Math.abs(velocityY);
+
+        if((velocityX > this.swipe_Min_Velocity) && (xDistance > this.swipe_Min_Distance)){
+            if(e1.getX() > e2.getX())
+                this.listener.onSwipe(SWIPE_LEFT);
+            else
+                this.listener.onSwipe(SWIPE_RIGHT);
+            return true;
+        }
+        else if((velocityY > this.swipe_Min_Velocity) && (yDistance > this.swipe_Min_Distance)){
+            if(e1.getY() > e2.getY())
+                this.listener.onSwipe(SWIPE_UP);
+            else
+                this.listener.onSwipe(SWIPE_DOWN);
+            return true;
+        }
+    }
+
+    // Triggers some sort of tap indicator if it's a tap.
+    @Override
+    public boolean onSingleTapUp(MotionEvent e){
+        this.tapIndicator = true;
+        return false;
+    }
+
+
+    // NFI what this does.
+    @Override
+    public boolean onDoubleTap(MotionEvent arg){
+        this.listener.onDoubleTap();
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent arg){
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent arg){
+
+        if(this.mode == MODE_DYNAMIC){
+            arg.setAction(ACTION_FAKE);
+            this.context.dispatchTouchEvent(arg);
+        }
+        return false;
+    }
+
+    static interface SimpleGestureListener{
+        void onSwipe(int direction);
+        void onDoubleTap();
+    }
+
+
 }
