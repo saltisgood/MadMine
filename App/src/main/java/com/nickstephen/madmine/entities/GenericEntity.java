05100b94e5cb86ae7aca18e46de5a55dec4a4633
Maddino <ben.madden.13@gmail.com>
2014-04-23 13:33:48 +1000

    Added some entity movement code in genericEntity and Spider.
    Early, basic changes so far.

--- App/src/main/java/com/nickstephen/madmine/entities/GenericEntity.java
+++ App/src/main/java/com/nickstephen/madmine/entities/GenericEntity.java
@@ -8,6 +8,17 @@ import com.nickstephen.madmine.util.Position;
 public class GenericEntity {
     protected Position pos;
 
+    /**
+     * Moves an entity to a new position by altering the position value of the entity and also by altering the map to account for its movement.
+     * @param movingEntity The entity that is going to be moved.
+     * @param newPosition The position to which the entity will be moved.
+     */
+    public void moveEntity(GenericEntity movingEntity, Position newPosition){
+        // Changes the entity's position.
+        movingEntity.pos = newPosition;
+        // TODO: Interact with the map.
+    }
+
 
     /**
      * A getter for the position of the entity.
--- App/src/main/java/com/nickstephen/madmine/entities/Spider.java
+++ App/src/main/java/com/nickstephen/madmine/entities/Spider.java
@@ -6,7 +6,14 @@ import com.nickstephen.madmine.util.Position;
  * Created by Ben on 22/04/2014.
  */
 public class Spider extends GenericNPC{
-    // TODO: Spider class.
+    void onTick(){
+        // TODO: Kill player?  I think this is done elsewhere though so maybe not required here.
+        moveAttempt();
+
+
+
+    }
+
     void moveAttempt(){
         // TODO: Spider movement.
         /*
@@ -15,11 +22,14 @@ public class Spider extends GenericNPC{
         If the spider can't walk forwards, it should turn right, then wait 2/3 sec.
          */
 
+        // Sets the desired position to be the space directly in front of the spider.
         Position desiredPos = this.pos.getRelPos(directionFacing);
         if (spaceAvailable(desiredPos)) { // This is meant to check to see if the space can be moved into.
-            // moveEntity(this, desiredPos);
+            // Moves the spider forward one block if there is room.
+            moveEntity(this, desiredPos);
         }
         else {
+            // If there is no room for the spider to move forwards, it turns pi/2 clockwise.
             // TODO: Will this turn the spider, or do I have to go this.directionFacing = this.directionFacing.rotateClockwise()?
             this.directionFacing.rotateClockwise();
         }
@@ -27,8 +37,13 @@ public class Spider extends GenericNPC{
 
     }
 
+    /**
+     * Checks to see if there is space available for the spider to move.
+     * @param newPos The position to which the spider will potentially move.
+     * @return
+     */
     boolean spaceAvailable(Position newPos){
-        // TODO: This is a placeholder method.
+        // TODO: This is currently a placeholder method.
         return true;
     }
 
